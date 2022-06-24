7c82314a8a6879083f19874bd69d60c2de217224
SHIN <zm255@naver.com>
2022-06-25 00:23:30 +0900

    day220624

---
+++ calc/appClientModule/calc/Cal.java
@@ -0,0 +1,12 @@
+package calc;
+
+import YoungBuildTEST.YoungBuildTest;
+
+public class Cal {
+	   public static void main(String[] args) {
+
+		      YoungBuildTest  KimShinYoung = new YoungBuildTest(4,2);
+		      KimShinYoung.getInfo();
+
+	   }
+}
