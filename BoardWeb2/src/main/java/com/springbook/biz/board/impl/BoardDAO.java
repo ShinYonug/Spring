cb9764bc107f8c187085b48cfd10a5a10e711622
SHIN <zm255@naver.com>
2022-07-07 17:41:03 +0900

    day220707

--- BoardWeb2/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ BoardWeb2/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -23,6 +23,9 @@ public class BoardDAO {
 	private final String BOARD_DELETE = "delete board where seq=?";
 	private final String BOARD_GET = "select * from board where seq=?";
 	private final String BOARD_LIST = "select * from board order by seq desc";
+	private final String BOARD_LIST_T = "select * from board where title like '%' || ? || '%' order by seq desc";
+	private final String BOARD_LIST_C = "select * from board where content like '%' || ? || '%' order by seq desc";
+	
 	private final String BOARD_CNT = "update board set cnt = cnt+1 where seq=?";
 	
 	public void insertBoard(BoardVO vo) {
@@ -97,7 +100,14 @@ public class BoardDAO {
 		List<BoardVO> boardList = new ArrayList<BoardVO>();
 		try {
 			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(BOARD_LIST);
+			//검색 조건
+			if(vo.getSearchCondition().equals("TITLE")) {
+			stmt = conn.prepareStatement(BOARD_LIST_T);
+			}else if(vo.getSearchKeyword().equals("CONTENT")) {
+				stmt = conn.prepareStatement(BOARD_LIST_C);
+			}//검색 조건 끝 
+//			stmt=conn.prepareStatement(BOARD_LIST);
+			stmt.setString(1, vo.getSearchKeyword());
 			rs = stmt.executeQuery();
 			while(rs.next()) {
 				BoardVO board = new BoardVO();
--- BoardWeb2/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardWeb2/src/main/java/com/springbook/view/board/BoardController.java
@@ -1,6 +1,9 @@
 package com.springbook.view.board;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
@@ -18,13 +22,27 @@ import com.springbook.biz.board.BoardVO;
 @SessionAttributes("board")
 public class BoardController {
 	@Autowired
-	private BoardService boardSevice;
+	private BoardService boardService;
 	
+	@RequestMapping("/dataTransform.do")
+	@ResponseBody
+	public List<BoardVO> dataTransform(BoardVO vo){
+		vo.setSearchCondition("TITLE");
+		vo.setSearchKeyword("");
+		List<BoardVO> boardList = boardService.getBoardList(vo);
+		return boardList;
+	}
 	
 //   글 등록
 	@RequestMapping("/insertBoard.do")
-	public String insertBoard(BoardVO vo) {
-		boardSevice.insertBoard(vo);
+	public String insertBoard(BoardVO vo) throws  IOException {
+		MultipartFile uploadFile = vo.getUploadFile();
+		if(!uploadFile.isEmpty()) {
+			String fileName = uploadFile.getOriginalFilename();
+			uploadFile.transferTo(new File("c:/Upload/"+fileName));
+		}
+		
+		boardService.insertBoard(vo);
 		return "getBoardList.do";
 	} // end of insertBoard
 
@@ -37,33 +55,33 @@ public class BoardController {
 		System.out.println("내용:" +vo.getContent());
 		System.out.println("등록일 " + vo.getRegDate());
 		System.out.println("조회수"+ vo.getCnt());
-		boardSevice.updateBoard(vo);
+		boardService.updateBoard(vo);
 		return "getBoardList.do";
 	} // end of updateBoard
 
 //   글 삭제
 	@RequestMapping("/deleteBoard.do")
 	public String deleteBoard(BoardVO vo) {
-		boardSevice.deleteBoard(vo);
+		boardService.deleteBoard(vo);
 		return "getBoardList.do";
 	} // end of deleteBoard
 
 //   글 상세 조회
 	@RequestMapping("/getBoard.do")
 	public String getBoard(BoardVO vo, Model model) {
-		model.addAttribute("board", boardSevice.getBoard(vo));
+		model.addAttribute("board", boardService.getBoard(vo));
 		return "getBoard.jsp";
 	} // end of getBoard
 
 //   글 목록 검색
 	@RequestMapping("/getBoardList.do")
 	public String getBoardList(
-			@RequestParam(value = "searchCondition", defaultValue = "TITLE", required = false) String condition,
-			@RequestParam(value = "searchKeyword", defaultValue = "", required = false) String keyword,
+//			@RequestParam(value = "searchCondition", defaultValue = "TITLE", required = false) String condition,
+//			@RequestParam(value = "searchKeyword", defaultValue = "", required = false) String keyword,
 			BoardVO vo, Model model) {
-		System.out.println("검색 조건: " + condition);
-		System.out.println("검색 단어: " + keyword);
-		model.addAttribute("boardList", boardSevice.getBoardList(vo));
+		if(vo.getSearchCondition() == null) vo.setSearchCondition("TITLE");
+		if(vo.getSearchKeyword()== null) vo.setSearchKeyword("");
+		model.addAttribute("boardList", boardService.getBoardList(vo));
 		return "getBoardList.jsp";
 
 	} // end of getBoardList
